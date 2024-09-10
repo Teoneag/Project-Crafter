37f7f44103f74de43c7cbb87bab6a59280d75ec6
Teoneag <teodor.neagoe.climber@gmail.com>
2024-09-10 20:08:04 +0200

    26 - refactor cli

--- src/main.py
+++ src/main.py
@@ -1,42 +1,45 @@
 import sys
 from init import init
 from analyze import analyze
-from rename import rename 
+from rename import rename
+
+commands = {
+    "init": {
+        "args": ["project-type", "project-name"],
+        "func": lambda args: init(args[0], args[1])
+    },
+    "analyze": {
+        "args": [],
+        "func": lambda args: analyze()
+    },
+    "rename": {
+        "args": ["new-name"],
+        "func": lambda args: rename(args[0])
+    }
+}
 
 def help():
-    print("""
-Available commands:
-    help
-    init
-    analize
-    rename
-""")
+    print("Available commands:")
+    for cmd, info in commands.items():
+        arg_list = " ".join(f"<{arg}>" for arg in info["args"])
+        print(f"    {cmd} {arg_list}")
+    print("    help")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    # invalid command
+    if len(sys.argv) < 2 or sys.argv[1] not in commands:
         print("Invalid command.")
         help()
         sys.exit(1)
+
+    command = sys.argv[1]
+    args = sys.argv[2:]
+    expected_args = commands[command]["args"]
     
-    option = sys.argv[1]
-    if option == "help":
-        help()
-    elif option == "init":
-        if len(sys.argv) != 4:
-            print("Incorrect args. Run: project init <project-type> <project-name>")
-            sys.exit(1)
-        init(sys.argv[2], sys.argv[3])
-    elif option == "analyze":
-        if len(sys.argv) != 2:
-            print("Incorrect args. Run: project analize")
-            sys.exit(1)
-        analyze()
-    elif option == "rename":
-        if len(sys.argv) != 4:
-            print("Incorrect args. Run: project rename <new-name>")
-            sys.exit(1)
-        rename(sys.argv[2], sys.argv[3])
-    else:
-        print("Invalid command.")
-        help()
+    # invalid nr of arguments
+    if len(args) != len(expected_args):
+        print(f"Usage: project {command} " + " ".join(f"<{arg}>" for arg in expected_args))
         sys.exit(1)
+    # good command: run it
+    else:
+        commands[command]["func"](args)
